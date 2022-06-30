b632102ed3616ca9ef84896e83f046d0a99c9cb7
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-06-30 15:26:12 +0100

    Update Mittenlib to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
 
 //    compileOnly("net.luckperms:api:5.4")
 
-    val mittenLibVersion = "1.0.16-SNAPSHOT"
+    val mittenLibVersion = "1.0.23-SNAPSHOT"
     shade("me.bristermitten:mittenlib-core:$mittenLibVersion")
     shade("me.bristermitten:mittenlib-minimessage:$mittenLibVersion")
     shade("me.bristermitten:mittenlib-papi:$mittenLibVersion")
